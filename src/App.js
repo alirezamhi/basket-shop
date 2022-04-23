df596e7db9b533336a93fd1b34266c7617cf1a95
alireza mohammadkhani <alirezamohammadkhani2@gmail.com>
2022-04-23 20:16:28 +0430

    step 5

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Productcontextprovider from "./context/Productcontextprovider";
 import Store from "./components/Store";
 import {Route,Redirect,Switch } from "react-router-dom";
 import Productdetail from "./components/Productdetail";
-
+import Cardcontextprovider from "./context/Cardcontextprovider";
 function App() {
 
   return (
     <div>
       <Productcontextprovider>
-        <Switch>
-          <Route path="/products/:id" component={Productdetail}/>
-          <Route path="/products" component={Store}/>
-          <Redirect to="/products"/>
-        </Switch>
+        <Cardcontextprovider>
+          <Switch>
+            <Route path="/products/:id" component={Productdetail}/>
+            <Route path="/products" component={Store}/>
+            <Redirect to="/products"/>
+          </Switch>
+        </Cardcontextprovider>
       </Productcontextprovider>
     </div>
   );
--- src/context/Cardcontextprovider.js
+++ src/context/Cardcontextprovider.js
@@ -1,4 +1,4 @@
-import React , { useReducer } from 'react';
+import React , { createContext, useReducer } from 'react';
 
 
 const initialState={
@@ -17,14 +17,56 @@ const cartReducer = (state,action) => {
                     quantity:1
                 })
             }
+            return {
+                ...state,
+                selecedItems : [...state.selecedItems]
+            }
+        case "REMOVE_ITEM":
+            const newstate = state.selecedItems.filter(item=>item.id !== action.payload.id)
+            return {
+                ...state,
+                selecedItems : [...newstate]
+            }
+        case "INCREASE":
+            const indexI = state.selecedItems.findIndex(item => item.id === action.payload.id);
+            state.selecedItems[indexI].quantity++;
+            return {
+                ...state
+            }
+        case "DECREASE":
+            const indexD = state.selecedItems.findIndex(item => item.id === action.payload.id);
+            state.selecedItems[indexD].quantity--;
+            return {
+                ...state
+            }
+        case "CHECKOUT":
+            return{
+                selecedItems : [],
+                itemCounter : 0,
+                total : 0,
+                checkout : true 
+            }
+        case "CLEARE":
+            return{
+                selecedItems : [],
+                itemCounter : 0,
+                total : 0,
+                checkout : false   
+            }
+        default:
+            return state;
     }
 }
 
-const Cardcontextprovider = () => {
+export const cardcontext = createContext();
+
+const Cardcontextprovider = ({children}) => {
     const[state , dispatch] = useReducer(cartReducer,initialState);
     return (
         <div>
-            
+            <cardcontext.Provider value={{state,dispatch}}>
+                {children}
+            </cardcontext.Provider>
         </div>
     );
 };
